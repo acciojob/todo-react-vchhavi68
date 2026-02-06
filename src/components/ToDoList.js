54590878cdcd62d87a7a7543e1cf01c9983f3c4d
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 19:51:47 +0530

    Implement basic To-Do List functionality

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,1 +1,40 @@
+import React, { useState } from "react";
+import "./todolist.css";
 
+function ToDoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <p>To-Do List</p>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <div className="item">
+            <li key={index}>
+              {todo}
+            </li>
+            <button onClick={() => handleRemoveTodo(index)}>Delete</button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
